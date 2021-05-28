fc110b7efa4f1468126b2d50ba164efea4e5a9e5
Georgely Jo Santino <georgelyjo@gmail.com>
2021-05-28 12:30:45 +0530

    commit

---
+++ dbconn.php
@@ -0,0 +1,11 @@
+
+<?php
+
+$db = mysqli_connect("localhost","id16823324_root","Georgely@123","id16823324_scoreboard");
+
+if(!$db)
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>score</title>
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+      <link rel="stylesheet" type="text/css" href="asset/css/board.css">
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+      <link href="asset/css/css2" rel="stylesheet">
+      <link rel="shortcut icon" href="/static/favicon.ico">
+      <link rel="manifest" href="/static/manifest.json">
+      <meta name="robots" content="noindex, follow">
+      <meta property="og:title" content="score">
+      <meta property="og:description" content="Created with Keepthescore.co, the free scoreboard and leaderboard tool">
+      <meta property="og:type" content="article">
+      <meta property="og:url" content="https://keepthescore.co/board/tfewrxyuqrr/">
+      <meta property="og:image" content="https://keepthescore.co/preview_image/tfewrxyuqrr.png">
+      <meta name="twitter:card" content="summary_large_image">
+      <meta name="twitter:image" content="https://keepthescore.co/preview_image/tfewrxyuqrr.png">
+      <meta name="twitter:title" content="score">
+      <meta name="twitter:description" content="Created with Keepthescore.co, the free scoreboard and leaderboard tool">
+      <style>
+         :root {
+         --primary-color: rgb(255, 252, 252);
+         --secondary-color: rgb(255, 252, 252);
+         --background-color: rgb(0, 0, 0);
+         --h1-font: 700 50px 'Montserrat', sans-serif;
+         --secondary-shadow: 3px 5px 7px 1px rgba(0, 0, 0, .05);
+         --border-radius: 5px;
+         }
+         body {
+         background-color: var(--background-color);
+         background-image: var(--background-image);
+         margin-bottom: 0; 
+         padding-bottom: 0;
+         }
+         #full-monty { 
+         color: var(--primary-color);
+         font-family: var(--body-font, 'Roboto', sans-serif) !important;
+         }
+         h1 {
+         margin: 30px 15px 5px 15px;
+         color: var(--primary-color);
+         font: var(--h1-font);
+         text-shadow: var(--primary-shadow)
+         }
+         #full-monty.p {
+         color: var(--primary-color);
+         }
+         .score-panel {
+         background-color: inherit;
+         border: 1px solid var(--secondary-color);
+         margin: 10px;
+         animation-iteration-count: 5;
+         min-width: 15%;
+         box-shadow: var(--secondary-shadow);
+         }
+         table {
+         background-color: var(--background-color);
+         }
+         .table-responsive {
+         border: 1px solid var(--secondary-color);
+         border-radius: var(--border-radius, 5px);
+         box-shadow: var(--secondary-shadow);
+         }
+         .leaderboard-table {
+         font-size: 25px;
+         }
+         .list-group-item {
+         border: 1px solid var(--secondary-color);
+         background-color: var(--background-color);
+         color: var(--primary-color);
+         box-shadow: var(--secondary-shadow);
+         font-size: 20px;
+         }
+         a.list-group-item {
+         color: var(--primary-color);
+         }
+         .table-responsive > table {
+         margin-bottom: 0;
+         }
+         .panel-default>.panel-heading {
+         background-color: #00000008;
+         color: var(--primary-color);
+         font-family: var(--h1-font);
+         }
+         .panel, panel-body {
+         background-color: var(--background-color);
+         color: var(--primary-color);
+         font-size: 30px;
+         }
+         .add-scores-button {
+         margin: 25px 15px 25px 15px;
+         }
+         .share-call-to-action a {
+         margin-bottom: 15px;
+         color: var(--primary-color);
+         }
+         .header-image {
+         margin-top: 30px;
+         }
+         .center-y {
+         vertical-align: middle !important;
+         }
+         .profile-img {
+         border: 1px solid var(--secondary-color);
+         text-shadow: var(--secondary-shadow);
+         object-fit: cover; 
+         width: 70px;
+         height: 70px;
+         margin: 0 10px 0 0;
+         }
+         .profile-img-scoreboard-compact {
+         float: left;
+         }
+         .profile-img-leaderboard-compact {
+         }
+         .profile-img-leaderboard-table {
+         float: left;
+         }
+         .profile-img-scoreboard-table {
+         }
+         .score-meta-data {
+         color: var(--secondary-color);
+         font-size: 20px;
+         text-shadow: var(--secondary-shadow);
+         }
+         @media (max-width: 600px) { 
+         .profile-img {
+         width: 40px;
+         height: 40px;
+         }
+         }
+      </style>
+      <script>
+         window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+         ga('create', 'UA-35934020-6', 'auto');
+         ga('set', 'anonymizeIp', true);
+         // Plugins that autotrack.js uses
+         ga('require', 'eventTracker');
+         ga('require', 'outboundLinkTracker');ga('set', 'dimension1', 'type=LEADERBOARD show_total=True trophy=HIGHEST private_game=False show_comments=False layout=TABLE view=read_only custom_colour_game=True split_test_group=5 is_upgraded=False ads=false');ga('send', 'pageview');
+      </script><script async src="asset/js/analytics.js"></script><script async src="asset/js/autotrack.js"></script><script src="asset/js/script.js" data-site="VNPUMDEN" defer></script>
+      <link rel="canonical" href="https://keepthescore.co/board/tfewrxyuqrr/">
+   </head>
+   <body>
+      <div class="container">
+         <div id="full-monty">
+            <div class="text-center">
+               <h1 class="animate__animated animate__wobble">Scoreboard</h1>
+               <div class="score-meta-data" style="margin-bottom: 30px"><h3> <b class=" animate__animated animate__rubberBand">(Round one) </b></h3> </div>
+            </div>
+            <div>
+               <div class="row">
+                  <div class="col-md-offset-2 col-md-8">
+                     <div class="table-responsive">
+                        <table class="table leaderboard-table">
+                           <tr class="darker">
+                              <th class="col-xs-2 text-center">#</th>
+                              <th class="col-xs-5"></th>
+                              <th class="col-xs-3 text-center">Score</th>
+                           </tr>
+<?php
+
+include "dbconn.php"; // Using database connection file here
+$x = 1;
+$records = mysqli_query($db,"select * from score1 ORDER BY score desc");// fetch data from database
+while($data = mysqli_fetch_array($records))
+{
+?>
+    
+                           <tr class="lighter animate__animated animate__tada" style="background-color: None;                                    color: None !important;">
+                              <th class="center-y text-center"><?php echo $x++; ?></th>
+                              <td class="is-editable score-cell-wrap center-y" id="panel-2997537"> <span id="player-2997537" style="color: var(--primary-color)"> <?php echo $data['name']; ?></span> </td>
+                              <td id="score-2997537" class="text-center center-y">  <?php echo $data['score'];?></td>
+                           </tr>
+                           
+
+
+<?php
+}
+         ?>
+         
+                        </table>
+                     </div>
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col-md-offset-2 col-md-8 board-comment-list">
+                     <div class="list-group"> </div>
+                  </div>
+               </div>
+            </div>
+            
+      </div>
+   </body>
+   <script type="text/javascript" src="asset/js/standard.js"></script> <script>
+      const api_url = "/api/tfewrxyuqrr/"
+      
+      
+   </script> 
+</html>
